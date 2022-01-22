68451d7a223d41e43624ec6c178229a7f3d4a347
afolabiopakunle <opakunleafolabi@gmail.com>
2022-01-22 13:58:13 +0100

    map used to create new array

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('hello!');
 
 const itemsArray = [
   {name: 'Bike', price: 100},
@@ -13,10 +12,42 @@ const itemsArray = [
   {name: 'Board', price: 99},
 ];
 
+const companies = [
+  {name: "Company One", category: "Finance", start: 1981, end: 2003},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2003},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989},
+]
+
+
 //filter Array
-console.log(itemsArray.length)
-const cheapStuffs = itemsArray.filter(item => {
-  return item.price <= 100
+
+ages = [42, 12, 13, 24, 30, 11, 8, 4, 23, 12, 21]
+
+// const affordableItems = itemsArray.filter(item => item.price <= 100)
+// const canDrink = ages.filter(age => age >= 21)
+// console.log(affordableItems);
+// console.log(canDrink);
+
+// const financeCompanies = companies.filter(company => company.category === 'Retail');
+// const eightiesCompanies = companies.filter(company => company.start >= 1980 && company.start < 1990)
+// console.log(financeCompanies)
+// console.log("Eighties Companies: ", eightiesCompanies);
+// const tenYearsCompanies = companies.filter(company => (company.end - company.start) >= 10)
+// console.log(tenYearsCompanies);
+const lessThan10 = [];
+const moreThan10 = [];
+companies.map(company => {
+  if((company.end - company.start) >= 10) {
+    moreThan10.push(company)
+    }  else {
+      lessThan10.push(company)
+    }
 })
-console.log(cheapStuffs.length)
-cheapStuffs.forEach(item => console.log(item.name))
+console.log("MORE: ", moreThan10)
+console.log("LESS: ", lessThan10)
